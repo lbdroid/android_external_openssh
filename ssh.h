cd52c0ecb7425b7535a89c4d81f24909be35838d
lbdroid <definesinsanity@gmail.com>
2016-04-22 13:31:24 -0400

    Fix compile on newer NDK

--- ssh.h
+++ ssh.h
@@ -101,7 +101,9 @@
 /* This is implemented in libc as "ndk_cruft", as a no-op
  * New platforms in L, starting with aarch64, do not have ndk_cruft,
  * so implement a no-op here */
-#ifdef __LP64__
+//#ifdef __LP64__
 // This was removed from BSD.
 #define arc4random_stir(x) do {} while (0);
-#endif
+//#endif
+# define endpwent() ((void)0)
+
